82f0965ee8c1e4f67b0e7b7d55ad08ed7c19a808
Cody Baker <cody.c.baker.phd@gmail.com>
2024-06-27 10:40:27 -0400

    saving state

--- src/leifer_lab_to_nwb/randi_nature_2023/interfaces/_pump_probe_segmentation_interface.py
+++ src/leifer_lab_to_nwb/randi_nature_2023/interfaces/_pump_probe_segmentation_interface.py
@@ -3,15 +3,11 @@ from typing import Literal
 
 import numpy
 import pandas
-from neuroconv.datainterfaces.ophys.basesegmentationextractorinterface import (
-    BaseSegmentationExtractorInterface,
-)
+from neuroconv.basedatainterface import BaseDataInterface
 from pynwb import NWBFile
 
 
-class PumpProbeSegmentationInterface(BaseSegmentationExtractorInterface):
-    ExtractorModuleName = "leifer_lab_to_nwb.randi_nature_2023.interfaces._pump_probe_segmentation_extractor"
-    ExtractorName = "PumpProbeSegmentationExtractor"
+class PumpProbeSegmentationInterface(BaseDataInterface):
 
     def __init__(self, *, pumpprobe_folder_path: str | pathlib.Path, channel_name: Literal["Green", "Red"] | str):
         """
@@ -22,37 +18,50 @@ class PumpProbeSegmentationInterface(BaseSegmentationExtractorInterface):
         pumpprobe_folder_path : DirectoryPath
             Path to the pumpprobe folder.
         """
+        super().__init__(pumpprobe_folder_path=pumpprobe_folder_path, channel_name=channel_name)
         pumpprobe_folder_path = pathlib.Path(pumpprobe_folder_path)
 
         # Other interfaces use CamelCase to refer to the NWB object the channel data will end up as
         # The files on the other hand are all lower case
         lower_channel_name = channel_name.lower()
-        pickle_file_path = pumpprobe_folder_path / f"{lower_channel_name}.pickle"
+        signal_file_path = pumpprobe_folder_path / f"{lower_channel_name}.pickle"
+        with open(file=signal_file_path, mode="rb") as io:
+            self.signal_info = pickle.load(file=io)
+        assert (
+            self.signal_info["info"]["method"] == "box"
+            and self.signal_info["info"]["ref_index"] == "30"
+            and self.signal_info["info"]["version"] == "1.5"
+        ), "Unimplemented method detected for mask type."
 
-        # TODO: generalize this timestamp extraction to a common utility function
-        # From prototyping data, the frameSync seems to start first...
-        sync_table_file_path = pumpprobe_folder_path / "other-frameSynchronous.txt"
-        sync_table = pandas.read_table(filepath_or_buffer=sync_table_file_path, index_col=False)
-        frame_indices = sync_table["Frame index"]
+        brains_file_path = pumpprobe_folder_path / "brains.json"
+        with open(brains_file_path, "r") as io:
+            self.brains_info = json.load(fp=io)
+
+        # Reshape coordinates to match more directly
+        reshaped_xyz_coordinates = []
+        counter = 0
+        for number_of_rois_in_volume in self.brains_info["nInVolume"]:
+            xyz_coordinates_per_volume = []
+            for volume_index in range(number_of_rois_in_volume):
+                xyz_coordinates_per_volume.append(
+                    (
+                        self.brains_info["coordZYX"][counter + volume_index][2],
+                        self.brains_info["coordZYX"][counter + volume_index][1],
+                        self.brains_info["coordZYX"][counter + volume_index][0],
+                    )
+                )
+
+            reshaped_xyz_coordinates.append(xyz_coordinates_per_volume)
+            counter += number_of_rois_in_volume
 
         # ...then the frameDetails has timestamps for a subset of the frame indices
         timestamps_file_path = pumpprobe_folder_path / "framesDetails.txt"
         timestamps_table = pandas.read_table(filepath_or_buffer=timestamps_file_path, index_col=False)
-        number_of_frames = timestamps_table.shape[0]
-
-        frame_count_delay = timestamps_table["frameCount"][0] - frame_indices[0]
-        frame_count_end = frame_count_delay + number_of_frames
-
-        sync_subtable = sync_table.iloc[frame_count_delay:frame_count_end]
 
         self.timestamps = numpy.array(timestamps_table["Timestamp"])
 
         # Hardcoding this for now
-        image_shape = (512, 512)
-        super().__init__(file_path=pickle_file_path, timestamps=self.timestamps, image_shape=image_shape)
-
-        # Hardcode a special plane segmentation name for this interface
-        # self.plane_segmentation_name = "PumpProbeSegmentation"
+        # image_shape = (512, 512)
 
     # def get_metadata(self) -> dict:
     #     metadata = super().get_metadata()
@@ -79,16 +88,12 @@ class PumpProbeSegmentationInterface(BaseSegmentationExtractorInterface):
     # def get_metadata_schema(self) -> dict:
     #     return super().get_metadata(photon_series_type="OnePhotonSeries")
 
-    # def add_to_nwbfile(
-    #     self,
-    #     *,
-    #     nwbfile: NWBFile,
-    #     metadata: dict | None = None,
-    #     stub_test: bool = False,
-    # ) -> None:
-    #     super().add_to_nwbfile(
-    #         nwbfile=nwbfile,
-    #         metadata=metadata,
-    #         stub_test=stub_test,
-    #         plane_segmentation_name="PumpProbeSegmentation",
-    #     )
+    def add_to_nwbfile(
+        self,
+        *,
+        nwbfile: NWBFile,
+        metadata: dict | None = None,
+        stub_test: bool = False,
+        stub_frames: int = 70,
+    ) -> None:
+        pass
